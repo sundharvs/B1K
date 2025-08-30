3ced51b207b84d5b63f9cb81050dc6897619ccae
Wensi Ai <wsai@stanford.edu>
2025-08-30 14:17:16 -0700

    Expose metadata in datas init

--- OmniGibson/omnigibson/learning/datas/__init__.py
+++ OmniGibson/omnigibson/learning/datas/__init__.py
@@ -1,7 +1,8 @@
 from .iterable_dataset import BehaviorIterableDataset
-from .lerobot_dataset import BehaviorLeRobotDataset
+from .lerobot_dataset import BehaviorLeRobotDataset, BehaviorLerobotDatasetMetadata
 
 __all__ = [
     "BehaviorIterableDataset",
     "BehaviorLeRobotDataset",
+    "BehaviorLerobotDatasetMetadata",
 ]
